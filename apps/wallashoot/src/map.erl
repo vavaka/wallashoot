234ab09148f0c19dd2a3d03a4858730d82103f2d
vavaka <vavaka.work@gmail.com>
2018-03-22 07:42:07 +1300

    WIP

--- apps/wallashoot/src/map.erl
+++ apps/wallashoot/src/map.erl
@@ -13,21 +13,22 @@
   new/2,
   width/1,
   height/1,
-  get/2,
-  set/3,
-  validate_position/2,
-  validate_direction/1,
-  next_position/2,
-  dump/3
+  is_position_in_bounds/2,
+  get_object_position/2,
+  set_object_position/3,
+  get_object_at/3,
+  delete_object/2
 ]).
 
 -record(map, {
-  width   :: non_neg_integer(),
-  height  :: non_neg_integer(),
-  surface :: array:array()
+  bounds  :: {non_neg_integer(), non_neg_integer()},
+  obj_pos :: map(),
+  pos_obj :: map()
 }).
 
 -type game_map() :: #map{}.
+
+-define(IS_POSITION_IN_BOUNDS(X, Y, Width, Height), X > 0 andalso X =< Width andalso Y > 0 andalso Y =< Height).
 
 %% ---------------------------------------------------------------
 %% API functions
@@ -36,86 +37,49 @@
 -spec new(Width :: non_neg_integer(), Height :: non_neg_integer()) -> game_map().
 new(Width, Height) ->
   #map{
-    width = Width,
-    height = Height,
-    surface = array:new(Height, [{default, array:new(Width, [{default, free}])}])
+    bounds = {Width, Height},
+    obj_pos = #{},
+    pos_obj = #{}
   }.
 
 -spec width(Map :: game_map()) -> non_neg_integer().
-width(#map{width = Width}) -> Width.
+width(#map{bounds = {Width, _}}) -> Width.
 
 -spec height(Map :: game_map()) -> non_neg_integer().
-height(#map{height = Height}) -> Height.
+height(#map{bounds = {_, Height}}) -> Height.
 
--spec get(Position :: position(), Map :: game_map()) -> any().
-get(Position = {X, Y}, Map = #map{surface = Surface}) ->
-  case validate_position(Position, Map) of
-    {invalid, Reason} ->
-      {error, {Reason, Position}};
-    valid ->
-      Row = array:get(Y, Surface),
-      array:get(X, Row)
+is_position_in_bounds({X, Y}, #map{bounds = {Width, Height}}) ->
+  X > 0 andalso X =< Width andalso Y > 0 andalso Y =< Height.
+
+-spec get_object_position(Object :: any(), Map :: game_map()) -> position().
+get_object_position(Object, #map{obj_pos = ObjPos}) ->
+  maps:get(Object, ObjPos).
+
+-spec set_object_position(Position :: position(), Object :: any(), Map :: game_map()) -> game_map().
+set_object_position(NewPosition, Object, Map = #map{obj_pos = ObjPos, pos_obj = PosObj}) ->
+  check_position(NewPosition, Map),
+
+  PosObjWithoutCurrentPosition = case maps:get(Object, ObjPos, undefined) of
+    undefined -> PosObj;
+    CurrentPosition -> maps:remove(CurrentPosition, PosObj)
+  end,
+
+  NewPosObj = maps:put(NewPosition, Object, PosObjWithoutCurrentPosition),
+  NewObjPos = maps:put(Object, NewPosition, ObjPos),
+  Map#map{obj_pos = NewObjPos, pos_obj = NewPosObj}.
+
+-spec get_object_at(Position :: position(), Map :: game_map(), Default :: term()) -> any().
+get_object_at(Position, Map = #map{pos_obj = PosObj}, Default) ->
+  check_position(Position, Map),
+  maps:get(Position, PosObj, Default).
+
+-spec delete_object(Object :: any(), Map :: game_map()) -> position().
+delete_object(Object, Map = #map{obj_pos = ObjPos, pos_obj = PosObj}) ->
+  Position = map:get_object_position(Object, Map),
+  Map#map{obj_pos = maps:remove(Object, ObjPos), pos_obj = maps:remove(Position, PosObj)}.
+
+check_position(Position, Map) ->
+  case is_position_in_bounds(Position, Map) of
+    true -> ok;
+    _ -> erlang:error(position_out_of_bounds)
   end.
-
--spec set(Position :: position(), Value :: any(), Map :: game_map()) -> game_map().
-set(Position = {X, Y}, Value, Map = #map{surface = Surface}) ->
-  case validate_position(Position, Map) of
-    {invalid, Reason} ->
-      {error, {Reason, Position}};
-    valid ->
-      Row = array:get(Y, Surface),
-      NewRow = array:set(X, Value, Row),
-      array:set(X, NewRow, Surface),
-      Map#map{surface = array:set(Y, NewRow, Surface)}
-  end.
-
--spec validate_position(Position :: position(), Map :: game_map()) -> validation_result().
-validate_position({X, Y}, #map{width = W, height = H}) when X < 0 orelse X >= W orelse Y < 0 orelse Y >= H ->
-  {invalid, position_out_of_range};
-validate_position(_Position, _Map) ->
-  valid.
-
--spec validate_direction(Direction :: direction()) -> validation_result().
-validate_direction(Direction) ->
-  case next_position(Direction, {0, 0}) of
-    {_X, _Y} ->
-      valid;
-    Error ->
-      {invalid, Error}
-  end.
-
--spec next_position(Direction :: direction(), Position :: position()) -> position() | invalid_direction | invalid_position.
-next_position(left, {X, Y}) ->
-  {X - 1, Y};
-next_position(right, {X, Y}) ->
-  {X + 1, Y};
-next_position(up, {X, Y}) ->
-  {X, Y - 1};
-next_position(down, {X, Y}) ->
-  {X, Y + 1};
-next_position(up_left, {X, Y}) ->
-  {X - 1, Y - 1};
-next_position(down_left, {X, Y}) ->
-  {X - 1, Y + 1};
-next_position(up_right, {X, Y}) ->
-  {X + 1, Y - 1};
-next_position(down_right, {X, Y}) ->
-  {X + 1, Y + 1};
-next_position(_, {_X, _Y}) ->
-  invalid_direction;
-next_position(_Direction, _) ->
-  invalid_position.
-
--spec dump(Encoding :: orddict:orddict(), Default :: string(), Map :: game_map()) -> list().
-dump(Encoding, Default, #map{surface = Surface}) ->
-  MapResult = array:foldl(fun(_Index1, Row, Acc) ->
-    RowResult = array:foldl(fun(_Index2, Value, Acc2) ->
-      Symbol = case orddict:find(Value, Encoding) of
-        error -> Default;
-        {ok, S} -> S
-      end,
-      [Symbol | Acc2]
-    end, [], Row),
-    [string:join(lists:reverse(RowResult), " ") | Acc]
-  end, [], Surface),
-  lists:reverse(MapResult).
